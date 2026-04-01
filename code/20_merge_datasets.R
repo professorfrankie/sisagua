b5bfb668fde0dc8f75bc73fd2c25693e4f188909
professorfrankie <h12329154@s.wu.ac.at>
2026-04-01 13:05:46 +0000

    merge dataset

--- code/20_merge_datasets.R
+++ code/20_merge_datasets.R
@@ -19,4 +19,9 @@ legal_amazon <- c("AC", "AP", "AM",  "MA", "MT", "PA", "RO", "RR", "TO")
 # Load data ---------------------------------------------------------------
 
 capture_points <- readRDS("/data/brazil_health/SISAGUA/capture_points_cleaned.RDS")
+sisagua_semestral <- readRDS("/data/brazil_health/SISAGUA/sisagua_semestral_cleaned.RDS")
 
+sisagua_semestral_cps_matched <- sisagua_semestral |>
+  left_join(capture_points, by = c("muni_code", "id_capture_point", "cnpj_office"))
+
+write_rds(sisagua_semestral_cps_matched, "/data/brazil_health/SISAGUA/sisagua_semestral_cps_matched.RDS")
